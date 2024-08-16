61b4038ce36544f258e99c38b8e870fddcc8a444
greg landrum <greg.landrum@gmail.com>
2024-08-16 12:51:45 +0200

    simplify

--- pubchem_shape/test_rdshapealign.py
+++ pubchem_shape/test_rdshapealign.py
@@ -8,29 +8,25 @@ datadir = pathlib.Path(__file__).parent.parent / 'test_data'
 
 class TestCase(unittest.TestCase):
 
-    def test1_Defaults(self):
+    def setUp(self):
         suppl = Chem.SDMolSupplier(datadir / 'test1.sdf')
-        ref = suppl[0]
-        probe = suppl[1]
-        tpl = rdShapeAlign.AlignMol(ref, probe)
+        self.ref = suppl[0]
+        self.probe = suppl[1]
+
+    def test1_Defaults(self):
+        tpl = rdShapeAlign.AlignMol(self.ref, self.probe)
         self.assertAlmostEqual(tpl[0], 0.773, places=3)
         self.assertAlmostEqual(tpl[1], 0.303, places=3)
 
     def test2_NoColor(self):
-        suppl = Chem.SDMolSupplier(datadir / 'test1.sdf')
-        ref = suppl[0]
-        probe = suppl[1]
-        tpl = rdShapeAlign.AlignMol(ref, probe, useColors=False)
+        tpl = rdShapeAlign.AlignMol(self.ref, self.probe, useColors=False)
         self.assertAlmostEqual(tpl[0], 0.773, places=3)
         self.assertAlmostEqual(tpl[1], 0.0, places=3)
 
     def test3_FromShape(self):
-        suppl = Chem.SDMolSupplier(datadir / 'test1.sdf')
-        ref = suppl[0]
-        probe = suppl[1]
-        shp = rdShapeAlign.PrepareConformer(ref)
+        shp = rdShapeAlign.PrepareConformer(self.ref)
         self.assertTrue(type(shp) == rdShapeAlign.ShapeInput)
-        tpl = rdShapeAlign.AlignMol(shp, probe)
+        tpl = rdShapeAlign.AlignMol(shp, self.probe)
         self.assertAlmostEqual(tpl[0], 0.773, places=3)
         self.assertAlmostEqual(tpl[1], 0.303, places=3)
 
